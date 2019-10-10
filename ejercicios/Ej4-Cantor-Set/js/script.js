07f6ed8ea94fd7551d5c4d86f891e05c081e887a
Vittorio Retrivi <retrovitto@gmail.com>
2019-10-10 00:10:53 -0300

    minor changes and finished cantor set

--- ejercicios/Ej4-Cantor-Set/js/script.js
+++ ejercicios/Ej4-Cantor-Set/js/script.js
@@ -1,1 +1,21 @@
+const drawLine = (x1, x2, y) => {
+  ctx.lineWidth = 10;
+  ctx.strokeStyle = "#ffffff";
+  ctx.beginPath();
+  ctx.lineTo(x1, y);
+  ctx.lineTo(x2, y);
+  ctx.stroke();
+}
 
+const cantor = (x1, x2, y, counter) => {
+  if (counter > 0) {
+    drawLine(x1, x2, y);
+    let ax = x1 + ((x2 - x1) * 1 / 3);
+    let bx = x1 + ((x2 - x1) * 2 / 3);
+    let y2 = y + 35;
+    cantor(x1, ax, y2, counter - 1);
+    cantor(bx, x2, y2, counter - 1);
+  }
+}
+
+cantor(0, width, 10, 10);
---
+++ ejercicios/Ej4-Cantor-Set/js/utils.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let width, height;
+
+const resetCanvas = () => {
+  let cw = document.documentElement.clientWidth;
+  let ch = document.documentElement.clientHeight
+  let s = cw > ch ? ch : cw;
+  width = canvas.width = s * 0.7;
+  height = canvas.height = s * 0.7;
+  ctx.clearRect(0, 0, width, height);
+}
+resetCanvas();
+
+window.addEventListener('resize', function() {
+  resetCanvas();
+  cantor(0, width, 10, 10);
+});
